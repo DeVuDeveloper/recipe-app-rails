92e219096e6dca3ed804963e1c24a34170339033
VuDej <drvu47@gmail.com>
2022-05-16 12:36:22 +0200

    added recipes index.html.erb file for diplay recipes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,3 @@
 Rails.application.routes.draw do
-  resources :recipe_foods
-  resources :foods
-  resources :recipes
-  resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :recipes, only: %i[index new create destroy show]
 end
