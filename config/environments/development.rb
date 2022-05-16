4b70c48b715b1687ebda92c99f09fd20c737eda4
VuDej <drvu47@gmail.com>
2022-05-16 17:05:36 +0200

    confirmable to real email adress made and customized all devise forms

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -45,7 +45,18 @@ Rails.application.configure do
   config.active_support.deprecation = :log
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'drvu47@gmail.com',
+    password:             'ablhvmendxdrdveu',
+    authentication:       'plain',
+    enable_starttls_auto: true,
+    open_timeout:         5,
+    read_timeout:         5 
+  }
   # Raise exceptions for disallowed deprecations.
   config.active_support.disallowed_deprecation = :raise
 
