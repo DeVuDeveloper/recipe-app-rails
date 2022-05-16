168c527405021f49421f5a6ee550348c44248c96
VuDej <drvu47@gmail.com>
2022-05-16 16:36:55 +0200

    installed gem devise, fixed errors with rails 7 altering Devise initializer configurations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    # Redirests signing out users back to sign-in
+    get "users", to: "devise/sessions#new"
+  end
+  devise_for :users
+
+  root 'recipes#index'
   resources :recipes, only: %i[index new create destroy show]
   resources :foods, only: %i[index new create destroy]
 end
