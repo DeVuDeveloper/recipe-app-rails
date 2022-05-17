f6290bdfa5457cf574b8fc118463da22f342c85a
VuDej <drvu47@gmail.com>
2022-05-17 15:45:02 +0200

    generated controller Publics and set route and index.html.erb.file

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  resources :publics
   devise_scope :user do
     # Redirests signing out users back to sign-in
     get "users", to: "devise/sessions#new"
   end
   devise_for :users
-
   root 'recipes#index'
   resources :recipes, only: %i[index new create destroy show]
+  resources :foods, only: %i[index new create destroy]
+  resources :recipe_foods, only: %i[new edit create destroy update]
+  resources :publics, only: [:index]
+
 end
