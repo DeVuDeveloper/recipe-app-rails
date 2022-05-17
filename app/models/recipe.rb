81353bb5971f90a37dc4200f90977f648f1d2681
VuDej <drvu47@gmail.com>
2022-05-17 20:31:55 +0200

    added functionality destroy recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,7 @@
 class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods, dependent: :destroy
-  
+
   validates :name, presence: true
   validates :description, presence: true
   validates :cooking_time, presence: true
