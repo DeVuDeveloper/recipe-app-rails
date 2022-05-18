7fce8c0635ab18c9f723f242d9712992bf50e7ca
VuDej <drvu47@gmail.com>
2022-05-18 17:51:36 +0200

    required changes made

--- test/controllers/shopping_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class ShoppingControllerTest < ActionDispatch::IntegrationTest
-  test 'should get list' do
-    get shopping_list_url
-    assert_response :success
-  end
-end
