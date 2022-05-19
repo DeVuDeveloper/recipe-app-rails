01fbaf8ff57574217a57febb6935e2a05919c915
VuDej <drvu47@gmail.com>
2022-05-19 07:27:02 +0200

    fix deploying

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,7 +44,9 @@ Rails.application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = {:host => 'myapp.herokuapp.com', :protocol => 'http'}
+  config.action_mailer.perform_deliveries = true
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
